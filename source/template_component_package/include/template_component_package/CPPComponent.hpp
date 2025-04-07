e8b41135af969c0da540df50bc5eeed39d70ce73
Dominic Reber <dominic@aica.tech>
2025-04-07 09:41:13 +0200

    feat: promote on step callback for components

--- source/template_component_package/include/template_component_package/CPPComponent.hpp
+++ source/template_component_package/include/template_component_package/CPPComponent.hpp
@@ -10,6 +10,8 @@ public:
 protected:
   bool on_validate_parameter_callback(const std::shared_ptr<state_representation::ParameterInterface>& parameter) override;
 
-  bool on_execute_callback() override;
+  void on_step_callback() override;
+
+  // bool on_execute_callback() override;
 };
 }  // namespace template_component_package
--- source/template_component_package/template_component_package/py_component.py
+++ source/template_component_package/template_component_package/py_component.py
@@ -10,12 +10,17 @@ class PyComponent(Component):
     def on_validate_parameter_callback(self, parameter: sr.Parameter) -> bool:
         # validate an incoming parameter value according to some criteria
         return True
+    
+    def on_step_callback(self):
+        # do something periodically
+        pass
 
-    def on_execute_callback(self) -> bool:
-        # If the component needs to do any post-construction behavior, invoke `self.execute()`
-        # at the end of the constructor, which will trigger this callback in a separate thread.
-        # This is only necessary when the behavior would otherwise block the constructor from completing
-        # in a timely manner, such as some time-intensive computation or waiting for an external trigger.
+    # def on_execute_callback(self) -> bool:
+    #     # If the component needs to do any post-construction behavior, invoke `self.execute()`
+    #     # at the end of the constructor, which will trigger this callback in a separate thread.
+    #     # This is only necessary when the behavior would otherwise block the constructor from completing
+    #     # in a timely manner, such as some time-intensive computation or waiting for an external trigger.
+    #     # This behavoir is mutually exclusive with the periodic behavior of `on_step_callback`.
 
-        # return True if the execution was successful, False otherwise
-        return True
+    #     # return True if the execution was successful, False otherwise
+    #     return True
